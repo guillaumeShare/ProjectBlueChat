45a5822ec1cfb2039ba818c99a66d65d5265329b
GuillaumeShare <leignier@ece.fr>
2015-11-18 10:38:18 +0100

    first commit

--- app/src/main/java/com/example/guillaume/bluechat/MainActivity.java
+++ app/src/main/java/com/example/guillaume/bluechat/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.guillaume.bluechat;
-
+// jftyftugy
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
